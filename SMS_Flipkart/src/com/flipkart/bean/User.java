86a550b7e575c846572da5f5e5a0693450912b3f
Nikita <nikitagupta5566@gmail.com>
2020-06-11 21:36:12 +0530

    Initial Commit

---
+++ SMS_Flipkart/src/com/flipkart/bean/User.java
@@ -0,0 +1,33 @@
+package com.flipkart.bean;
+
+public class User {
+	
+	String role;
+	String username;
+	String password;
+	int id;
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+
+}
